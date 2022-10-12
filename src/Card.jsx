b0a27513ebd47486b29573aa3e0e7f4f0fece1ab
chris <chrishethertonweb@gmail.com>
2022-10-12 15:31:43 +0200

    designing card, bankName and number

---
+++ src/Card.jsx
@@ -0,0 +1,63 @@
+function Card() {
+  return (
+    <CardDetail
+      name="Oscar Pistorius"
+      expirationDate="08/09"
+      creditCardNumber="6777 7546 8907 1230"
+      bankName="Bank of England"
+    />
+  );
+}
+
+export default Card;
+
+function CardDetail(props) {
+  const { name, expirationDate, creditCardNumber, bankName } = props;
+
+  return (
+    <div
+      style={{
+        padding: "20px 20px",
+        display: "block",
+        padding: "20px, 10px",
+        width: "300px",
+        height: "200px",
+        borderRadius: "5%",
+        backgroundColor: "#4495fd",
+        color: "white",
+      }}
+    >
+      <p
+        style={{
+          paddingTop: "20px",
+          paddingRight: "20px",
+          textAlign: "right",
+          fontSize: "1.8rem",
+          fontWeight: "600",
+        }}
+      >
+        {bankName}
+      </p>
+      <p
+        style={{
+          fontSize: "1.2rem",
+          position: "relative",
+          textAlign: "center",
+          letterSpacing: "3px",
+          top: "20px",
+        }}
+      >
+        {creditCardNumber}
+      </p>
+      <p
+        style={{
+          fontSize: ".6rem",
+          position: "relative",
+          left: "38px",
+        }}
+      >
+        1234
+      </p>
+    </div>
+  );
+}
